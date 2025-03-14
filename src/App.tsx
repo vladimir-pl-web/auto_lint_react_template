567a42f9fd00e61936efba9c242678a78358c346
Vladimirs <vladimirplotnikov371@gmail.com>
2025-03-15 00:36:07 +0200

    comma

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import './App.css'
 import { Button } from './components/ui/button'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
